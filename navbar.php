9da293b2d6d7bab745a67cde1462f5792781cfd5
DBMD974 <135600167+DBMD974@users.noreply.github.com>
2023-07-18 21:21:05 +0400

    navbar rajouter fichier a part

---
+++ navbar.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Navbar</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="navbar">
+    <header>
+        <nav>
+            <div class="nav-left">
+                <img src="site_exemples/img/LOGO.gif" alt="Logo">
+            </div>
+            <div class="nav-center">
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+            </div>
+            <div class="nav-right">
+                <button class="btn">Inscription</button>
+                <button class="btn">Connexion</button>
+            </div>
+        </nav>
+    </header>
+    <!-- Le reste de ton contenu de blog ici -->
+
+    <script src="script.js"></script>
+</body>
+</html>

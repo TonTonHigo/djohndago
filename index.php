f9fb369c182fe1b96c9b43f455fd8469503a03bc
DBMD974 <135600167+DBMD974@users.noreply.github.com>
2023-07-19 14:24:32 +0400

    ok pour le font-family

--- index.php
+++ index.php
@@ -7,11 +7,20 @@
     <title>ACCEUIL</title>
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lilita+One&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Orbitron:wght@400;500&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lilita+One&family=Orbitron:wght@400;500&family=Permanent+Marker&display=swap" rel="stylesheet">
 </head>
 <body class="acceuil">
 <?php include ('header.php'); ?>
 
-
+<br>
 <!-- mon carousel -->
 
 <div class="carousel-container-nav">
@@ -25,14 +34,40 @@
             <img src="site_exemples\img\onepeace.jpg" alt="Image 7">
         </div>
     </div>
-
+<br>
 
     <hr class="trait">
 
-
+<br>
     <div class="gta">
     <img src="site_exemples\img\gta.jpg" alt="Image 2">
     </div>
+
+    <br>
+
+    <h2>ARTICLES RECENTS ...</h2>
+
+    <section class="card-section">
+    <div class="card">
+        <img src="site_exemples\img\nfs.jpg" alt="Image 1">
+        <h3>NEED FOR SPEEDS</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, illo!</p>
+        <button>Voir article</button>
+    </div>
+    <div class="card">
+        <img src="site_exemples\img\fps game image.jpg" alt="Image 2">
+        <h3>CALL OF DUTY</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, voluptatem!</p>
+        <button>Voir article</button>
+    </div>
+    <div class="card">
+        <img src="site_exemples\img\harleyqueen.jpg" alt="Image 3">
+        <h3>HARLEY QUEEN</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores, explicabo?</p>
+        <button>Voir article</button>
+    </div>
+</section>
+
 
 
     

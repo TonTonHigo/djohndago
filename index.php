13182b5bb591cbb17be45b11516460f7733a67a9
DBMD974 <135600167+DBMD974@users.noreply.github.com>
2023-07-19 10:03:57 +0400

    ajout de carousel

--- index.php
+++ index.php
@@ -11,6 +11,60 @@
 <body class="acceuil">
 <?php include ('header.php'); ?>
 
+<div class="carousel-container">
+        <div class="carousel">
+            <div class="slide">
+                <img src="site_exemples\img\fps game image.jpg" alt="Image 1">
+                <div class="caption">
+                    <h3>Titre de l'image 1</h3>
+                    <p class="fluo-text">Texte de l'image 1</p>
+                </div>
+            </div>
+            <div class="slide">
+                <img src="site_exemples\img\nfs.jpg" alt="Image 2">
+                <div class="caption">
+                    <h3>Titre de l'image 2</h3>
+                    <p class="fluo-text">Texte de l'image 2</p>
+                </div>
+            </div>
+            <div class="slide">
+                <img src="site_exemples\img\decormanette.jpg" alt="Image 2">
+                <div class="caption">
+                    <h3>Titre de l'image 2</h3>
+                    <p class="fluo-text">Texte de l'image 2</p>
+                </div>
+            </div>
+            <div class="slide">
+                <img src="site_exemples\img\decormarvel.jpg" alt="Image 2">
+                <div class="caption">
+                    <h3>Titre de l'image 2</h3>
+                    <p class="fluo-text">Texte de l'image 2</p>
+                </div>
+            </div>
+            <div class="slide">
+                <img src="site_exemples\img\queen.jpg" alt="Image 2">
+                <div class="caption">
+                    <h3>Titre de l'image 2</h3>
+                    <p class="fluo-text">Texte de l'image 2</p>
+                </div>
+            </div>
+            <div class="slide">
+                <img src="site_exemples\img\daysgone.jpg" alt="Image 2">
+                <div class="caption">
+                    <h3>Titre de l'image 2</h3>
+                    <p class="fluo-text">Texte de l'image 2</p>
+                </div>
+            </div>
+            <!-- Ajoute les autres slides ici -->
+        </div>
+    </div>
+
+    <hr class="trait">
+
+
+    <div class="gta">
+    <img src="site_exemples\img\gta.jpg" alt="Image 2">
+    </div>
 
 
     
--- script.js
+++ script.js
@@ -1,6 +1,21 @@
+// le carousel pour la page index
+const slides = document.querySelectorAll('.slide');
 
+let currentSlide = 0;
 
+function showSlide() {
+    slides.forEach((slide, index) => {
+        if (index === currentSlide) {
+            slide.classList.add('active');
+        } else {
+            slide.classList.remove('active');
+        }
+    });
 
+    currentSlide = (currentSlide + 1) % slides.length;
+}
+
+setInterval(showSlide, 4000);
 
 
 

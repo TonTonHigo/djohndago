fb46b558c4229bdbf8744b865488237b12c468dc
DBMD974 <135600167+DBMD974@users.noreply.github.com>
2023-07-21 14:29:10 +0400

    modal ok

--- header.php
+++ header.php
@@ -111,7 +111,7 @@
                          </div>
                          <div class="modal-footer">
                              <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button> -->
-                             <button form="inscription" type="submit" class="btn btn-primary">INSCRIPTION</button>
+                             <button form="inscription" type="submit" class="btn-modal">INSCRIPTION</button>
                          </div>
                      </div>
                  </div>

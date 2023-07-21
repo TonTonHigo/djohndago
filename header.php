3c057ad57a0356802dee985636eb00f30b4bae57
DBMD974 <135600167+DBMD974@users.noreply.github.com>
2023-07-21 15:00:14 +0400

    bouton modal modifier

--- header.php
+++ header.php
@@ -67,7 +67,7 @@
                          </div>
                          <div class="modal-footer">
                              <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annuler</button> -->
-                             <button form="connexion" type="submit" class="btn btn-primary">CONNEXION</button>
+                             <button form="connexion" type="submit" class="btn-modal">CONNEXION</button>
                          </div>
                      </div>
                  </div>

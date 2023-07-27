abaa45b3bcbcfc6f81b570b16afbb0fc04d6cb84
DBMD974 <135600167+DBMD974@users.noreply.github.com>
2023-07-27 14:11:15 +0400

    modification de mon index

--- index.php
+++ index.php
@@ -24,121 +24,8 @@
 <body class="acceuil">
     <?php include('header.php'); ?>
 
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <!-- mon carousel -->
-<!-- 
-    <div class="carousel-container-nav">
-        <div class="carousel-slide">
-            <img src="site_exemples\img\battlefield-2042-logo.webp" alt="Image 1">
-            <img src="site_exemples\img\daysgone.jpg" alt="Image 2">
-            <img src="site_exemples\img\wallpaperflare.com_wallpaper(10).jpg" alt="Image 3">
-            <img src="site_exemples\img\decolastofusblackandwhite.jpg" alt="Image 4">
-            <img src="site_exemples\img\onepeace.jpg" alt="Image 5">
-            <img src="site_exemples\img\sexy.jpg" alt="Image 6">
-            <img src="site_exemples\img\onepeace.jpg" alt="Image 7">
-        </div>
-    </div>
-    <br> -->
 
-    <hr class="trait">
-
-    <!-- <br> -->
-    <!-- <div class="image-container">
-        <img src="site_exemples\img\watchdog.jpg" alt="Votre image">
-    </div> -->
-
-    <!-- <br> -->
-
-    <!-- <h2>ARTICLES RECENTS ...</h2>
-
-    <section class="card-section">
-    <div class="card-image">
-        <img src="site_exemples\img\onepeace.jpg" alt="Image 1">
-        <div class="card-buttons">
-            <button>Voir article</button>
-            <button>Update</button>
-            <button>Delete</button>
-        </div>
-    </div>
-
-    <div class="card-image">
-        <img src="site_exemples\img\fps game image.jpg" alt="Image 1">
-        <div class="card-buttons">
-            <button>Voir article</button>
-            <button>Update</button>
-            <button>Delete</button>
-        </div>
-    </div>
-
-    <div class="card-image">
-        <img src="site_exemples\img\harleyqueen.jpg" alt="Image 1">
-        <div class="card-buttons">
-            <button>Voir article</button>
-            <button>Update</button>
-            <button>Delete</button>
-        </div>
-    </div> -->
-    <!-- Ajoutez d'autres images similaires ici -->
-<!-- </section>
-
-
-    <br>
-    <hr>
-    <br>
-
-    <h2>FPS ...</h2>
-
-    <-- seconde image -->
-    <!-- <div class="image-container">
-        <img src="site_exemples\img\call-of-duty-warzone-photo-1381831.webp" alt="Votre image">
-    </div>
-
-
-    <br>
-    <br>  -->
-
-
-    <!-- <section class="card-section">
-    <div class="card-image">
-        <img src="site_exemples\img\decor (29).jpg" alt="Image 1">
-        <div class="card-buttons">
-            <button>Voir article</button>
-            <button>Update</button>
-            <button>Delete</button>
-        </div>
-    </div>
-
-    <div class="card-image">
-        <img src="site_exemples\img\decor (15).jpg" alt="Image 1">
-        <div class="card-buttons">
-            <button>Voir article</button>
-            <button>Update</button>
-            <button>Delete</button>
-        </div>
-    </div>
-
-    <div class="card-image">
-        <img src="site_exemples\img\wallpaperflare.com_wallpaper(1).jpg" alt="Image 1">
-        <div class="card-buttons">
-            <button>Voir article</button>
-            <button>Update</button>
-            <button>Delete</button>
-        </div>
-    </div> -->
-    <!-- Ajoutez d'autres images similaires ici -->
-<!-- </section>
-
-    <br>
-    <hr>
-    <br> -->
-
+    
 
 
 
@@ -201,6 +88,9 @@ foreach ($fpsImages as $image) {
         <img src="' . $image . '" alt="Votre image">
     </div>';
 }
+
+
+
 
 
 ?>

6918bdd13273f284d665fbcf8f1e9b08fccd4796
TonTonHigo <hugoh420@outlook.fr>
2023-07-28 09:37:36 +0400

    lol

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php include('Maconnexion.php'); ?>
+<?php
+session_start();
+include('Maconnexion.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,8 +9,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ACCEUIL</title>
-     <!-- bootstrap -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -32,125 +35,79 @@
     <?php
 
 
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
 
 
 
 
 
-// Définir un tableau contenant les chemins d'accès des images
-$images = array(
-    "site_exemples\img\battlefield-2042-logo.webp",
-    "site_exemples\img\daysgone.jpg",
-    "site_exemples\img\wallpaperflare.com_wallpaper(10).jpg",
-    "site_exemples\img\decolastofusblackandwhite.jpg",
-    "site_exemples\img\onepeace.jpg",
-    "site_exemples\img\sexy.jpg",
-    "site_exemples\img\onepeace.jpg",
-);
+    // Définir un tableau contenant les chemins d'accès des images
+    $images = array(
+        "site_exemples\img\battlefield-2042-logo.webp",
+        "site_exemples\img\daysgone.jpg",
+        "site_exemples\img\wallpaperflare.com_wallpaper(10).jpg",
+        "site_exemples\img\decolastofusblackandwhite.jpg",
+        "site_exemples\img\onepeace.jpg",
+        "site_exemples\img\sexy.jpg",
+        "site_exemples\img\onepeace.jpg",
+    );
 
-// Boucle foreach pour afficher les images du carousel
-echo '<div class="carousel-container-nav">
+    // Boucle foreach pour afficher les images du carousel
+    echo '<div class="carousel-container-nav">
     <div class="carousel-slide">';
-foreach ($images as $image) {
-    echo '<img src="' . $image . '" alt="Image">';
-}
-echo '</div>
+    foreach ($images as $image) {
+        echo '<img src="' . $image . '" alt="Image">';
+    }
+    echo '</div>
 </div>';
 
 
-echo "<br>";
-echo "<br>";
-echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
 
 
-// Chemin d'accès de votre image que vous souhaitez afficher
-$chemin_image = "site_exemples\img\watchdog.jpg";
-echo '<div class="image-container">
+    // Chemin d'accès de votre image que vous souhaitez afficher
+    $chemin_image = "site_exemples\img\watchdog.jpg";
+    echo '<div class="image-container">
     <img src="' . $chemin_image . '" alt="Votre image">
 </div>';
 
-echo "<br>";
-echo "<br>";
-echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
 
 
 
 
 
-// Tableau des articles récents avec les boutons (à remplacer par vos données réelles)
-$articles = array(
-    array("site_exemples\img\onepeace.jpg"),
-    array("site_exemples\img\fps game image.jpg"),
-    array("site_exemples\img\harleyqueen.jpg"),
-    // Ajoutez d'autres articles similaires ici
-);
+    // Tableau des articles récents avec les boutons (à remplacer par vos données réelles)
+    $articles = array(
+        array("site_exemples\img\onepeace.jpg"),
+        array("site_exemples\img\fps game image.jpg"),
+        array("site_exemples\img\harleyqueen.jpg"),
+        // Ajoutez d'autres articles similaires ici
+    );
 
-// Boucle foreach pour afficher les cartes d'articles récents
-echo '<h2>ARTICLES RECENTS ...</h2>
+    // Boucle foreach pour afficher les cartes d'articles récents
+    echo '<h2>ARTICLES RECENTS ...</h2>
 <section class="card-section">';
-// On instancie une nouvelle MaConnexion qu'on met dans notre variable $trois_cartes 
-$trois_cartes = new MaConnexion("blog_jeux", "" , "root" , "localhost");
-// On utilise une fonction de la classe MaConnexion pour select les cartes et on les met dans la variable $afficher
-$afficher = $trois_cartes -> select_articles_recents("articles","*");
-// Maintenant qu'on à selectionner nos articles on doit les afficher donc on fait un foreach car $afficher est un tableau dans un tableau ($afficher = [[propriété:valeur])
-foreach($afficher as $cartes){
-    echo '<div class="card-image">
+    // On instancie une nouvelle MaConnexion qu'on met dans notre variable $trois_cartes 
+    $trois_cartes = new MaConnexion("blog_jeux", "", "root", "localhost");
+    // On utilise une fonction de la classe MaConnexion pour select les cartes et on les met dans la variable $afficher
+    $afficher = $trois_cartes->select_articles_recents("articles", "*");
+    // Maintenant qu'on à selectionner nos articles on doit les afficher donc on fait un foreach car $afficher est un tableau dans un tableau ($afficher = [[propriété:valeur])
+    foreach ($afficher as $cartes) {
+        echo '<div class="card-image">
         <img src="' . $cartes['image'] . '" alt="Image">
         <div class="card-buttons">
-            <button>Voir article</button>
-
-        </div>
-    </div>';
-}
-echo '</section>';
-
-echo "<br>";
-echo "<br>";
-echo "<br>";
-
-
-
-// Chemin d'accès de votre image que vous souhaitez afficher
-$chemin_image = "site_exemples\img\poi.webp";
-echo '<div class="image-container">
-    <img src="' . $chemin_image . '" alt="Votre image">
-</div>';
-
-echo "<br>";
-echo "<br>";
-echo "<br>";
-
-
-
-// Tableau des images de FPS (à remplacer par vos données réelles)
-$fpsImages = array(
-    "site_exemples\img\call-of-duty-warzone-photo-1381831.webp",
-    "site_exemples\img\decor (29).jpg",
-    "site_exemples/img/battlefield-2042-logo.webp",
-    // Ajoutez d'autres images de FPS similaires ici
-);
-
-// Boucle foreach pour afficher les images de FPS
-echo '<h2>FPS ...</h2>
-<section class="card-section">';
-$fpsImages = $trois_cartes -> select_articles_FPS("articles","*");
-foreach ($fpsImages as $cartes) {
-    echo '<div class="card-image">
-<<<<<<< HEAD
-    <img src="' . $cartes['image'] .
-        '" alt="Image">
-=======
-    <img src="' . $cartes['image'] . '" alt="Image">
->>>>>>> 9aadc08fe0be217c4742dbb28289cbb10e85edb2
-    <div class="card-buttons">
-    <form method="POST" action="article.php">
-                                    <input name="id_articles" value="';
+        <form method="POST" action="article.php">
+        <input name="id_articles" value="';
 
         if (isset($_SESSION['article']) && $_SESSION['article'] == "") {
             $_SESSION['article'] = $cartes['id_articles'];
@@ -160,27 +117,81 @@ foreach ($fpsImages as $cartes) {
             echo $cartes['id_articles'];
         }
         echo '
-                        " hidden>
-                                    <button type="submit" class="txt3 butcard">Voir l\'article</button>
-    </form>
-        
+                " hidden>
+        <button type="submit">Voir l\'article</button>
+        </form>
 
-    </div>
+        </div>
+    </div>';
+    }
+    echo '</section>';
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+
+
+    // Chemin d'accès de votre image que vous souhaitez afficher
+    $chemin_image = "site_exemples\img\poi.webp";
+    echo '<div class="image-container">
+    <img src="' . $chemin_image . '" alt="Votre image">
 </div>';
-}
-echo '</section>';
 
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
 
 
 
-?>
-<?php include('footer.php'); ?>
+    // Tableau des images de FPS (à remplacer par vos données réelles)
+    $fpsImages = array(
+        "site_exemples\img\call-of-duty-warzone-photo-1381831.webp",
+        "site_exemples\img\decor (29).jpg",
+        "site_exemples/img/battlefield-2042-logo.webp",
+        // Ajoutez d'autres images de FPS similaires ici
+    );
+
+    // Boucle foreach pour afficher les images de FPS
+    echo '<h2>FPS ...</h2>
+    <section class="card-section">';
+    $fpsImages = $trois_cartes->select_articles_FPS("articles", "*");
+    foreach ($fpsImages as $cartes) {
+        echo '<div class="card-image">
+        <img src="' . $cartes['image'] . '" alt="Image">
+        <div class="card-buttons">
+        <form method="POST" action="article.php">
+                                        <input name="id_articles" value="';
+
+        if (isset($_SESSION['article']) && $_SESSION['article'] == "") {
+            $_SESSION['article'] = $cartes['id_articles'];
+            echo $cartes['id_articles'];
+        } else {
+            unset($_SESSION['article']);
+            echo $cartes['id_articles'];
+        }
+        echo '
+                " hidden>
+            <button type="submit">Voir l\'article</button>
+        </form>
+
+
+        </div>
+    </div>';
+    }
+    echo '</section>';
+
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+
+
+
+    ?>
+    <?php include('footer.php'); ?>
 
 
 
@@ -189,9 +200,9 @@ echo "<br>";
 
 
 
-<!-- bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-    
+    <!-- bootstrap -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
     <script src="script.js"></script>
 </body>
 
